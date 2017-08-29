dce93828ceb1157ca5d10231c93cc6ac9dde8f17
Alexander Matveev <blackkorol@gmail.com>
2017-08-30 02:16:06 +0300

    navigation ?

--- src/MatchScreen.js
+++ src/MatchScreen.js
@@ -13,7 +13,7 @@ import {
   Image
 } from 'react-native';
 import { defaultStyles } from './styles';
-
+import Icon from 'react-native-vector-icons/Ionicons';
 import Participant from './Participant';
 
 export default class MatchScreen extends Component {
@@ -22,22 +22,19 @@ export default class MatchScreen extends Component {
     const { matches } = this.props.navigation.state.params;
     return (
       <View style={styles.container}>
-         {/*  <Text> { JSON.stringify(matches) }</Text> 
-            change to custom matchParticipant
-          <ScrollView
-            ref={(scrollView) => { this._scrollView = scrollView; }}  
-          >
-          { matches.map((participant, index) => {
-                return  (
-                  <Participant participant={participant} key={index} />  
-                );
-            })
-          } 
-          </ScrollView>
-         */}
+        <View style={styles.navBar}>
+          <Icon style={styles.navBarButtonIcon} onPress={() => this.props.navigation.goBack() } name="ios-arrow-back" size={25} color="#900"  />
+          <Text style={ [styles.navBarButton,{
+            fontWeight: 'bold'
+          }]} onPress={() => this.props.navigation.goBack() }>Назад к мероприятиям</Text>
+          {/* return to events where to find data ? */}
+          <Text style={styles.navBarHeader}></Text>
+          <Text style={styles.navBarButton}>  </Text> 
+        </View>
          
-
+        
         { matches.map((object, index) => {
+
             return object.map((participant, index) => {
               return  (
                 <View style={styles.containerPart} >
@@ -65,6 +62,36 @@ export default class MatchScreen extends Component {
 }
 
 const styles = StyleSheet.create({
+  navBar: {
+    flexDirection: 'row',
+    paddingTop: 30,
+    height: 64,
+    backgroundColor: '#FFFFFF' // '#1EAAF1'
+  },
+  navBarButton: {
+    color: '#262626',
+    textAlign:'center',
+    width: 200,
+    color: '#3f88fb'
+  },
+  navBarButtonIcon: {
+    marginTop: -4,
+    color: '#262626',
+    textAlign:'center',
+    marginLeft: 10,
+    // width: 200,
+    color: '#3f88fb'
+  },
+  navBarHeader: {
+    flex: 1,
+    color: '#262626',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    ...defaultStyles.text,
+    fontSize: 15,
+    // marginTop: 7
+  },
+  /////////
   containerPart: {
     flexDirection: 'row',
   },
