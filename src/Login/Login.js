962eb9f68d4c7b843d43c6794fe14e0611ed76ce
Alexander Matveev <blackkorol@gmail.com>
2017-10-04 09:48:03 +0300

    Comment

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -8,6 +8,9 @@ import {
   Dimensions,
   TouchableOpacity
 } from 'react-native';
+
+import AccountKit, { LoginButton, Color, StatusBarStyle } from 'react-native-facebook-account-kit'
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 import SafariView from 'react-native-safari-view';
 
@@ -59,8 +62,53 @@ export default class Login extends Component {
   // Handle Login with Vk button tap
   loginWithVk = () => this.openURL(`${URL}/auth/vkontakte`);
   
+
+  configureAccountKit() {
+    AccountKit.configure({
+      theme: {
+        //backgroundColor:       Color.rgba(0,120,0,0.1),
+        //buttonBackgroundColor: Color.rgba(0, 153, 0, 1.00),
+        //buttonDisabledBackgroundColor: Color.rgba(100, 153, 0, 0.5),
+        //buttonBorderColor:     Color.rgba(0,255,0,1),
+        //buttonTextColor:       Color.rgba(0,255,0,1),
+        //headerBackgroundColor: Color.rgba(0, 153, 0, 1.00),
+        //headerTextColor:       Color.rgba(0,255,0,1),
+        //headerButtonTextColor: Color.rgba(0,255,0,1),
+        //iconColor:             Color.rgba(0,255,0,1),
+        //inputBackgroundColor:  Color.rgba(0,255,0,1),
+        //inputBorderColor:      Color.hex('#ccc'),
+        //inputTextColor:        Color.hex('#0f0'),
+        //textColor:             Color.hex('#0f0'),
+        //titleColor:            Color.hex('#0f0'),
+        //backgroundImage:       "background.png",
+        //statusBarStyle:        StatusBarStyle.LightContent,
+      },
+      //countryWhitelist: [ "AR", "BR", "US" ],
+      //countryBlacklist: [ "BR" ],
+      defaultCountry: "RU",
+      // initialEmail: 'example.com',
+      initialPhoneCountryPrefix: '+7',
+      initialPhoneNumber: '',
+    })
+  }
+
   loginWithAccountKit = () => {
     console.log('loginWithAccountKit')
+    this.configureAccountKit();
+    AccountKit.loginWithPhone()
+    .then((token) => {
+      if (!token) {
+        console.log('Login cancelled')
+      } else {
+        console.log(`Logged with phone. Token: ${token}`)
+      }
+    })
+
+    
+    // AccountKit.getCurrentAccount().then(account => {
+    //   console.log(account);
+       
+    // })
   }
 
   // Open URL in a browser
